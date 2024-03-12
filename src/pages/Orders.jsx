b80d814d935e49e245b2b5d025324e3789003b33
libutanpaulo <libutanpaulo@gmail.com>
2024-03-12 20:25:59 +0800

    Updates to main changes

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -26,6 +26,6 @@ export default function Orders() {
   return isAdmin ? (
     <OrdersDataTable orders={orders} key={orders._id} />
   ) : (
-    <Navigate to="./" />
+    <Navigate to="/" />
   );
 }
